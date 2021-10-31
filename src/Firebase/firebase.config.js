ca4ad9fedf5b9f5d1e5dbc89a916dcc2904cb0d4
Najmul-Kabir-Joy <najmulkabir178@gmail.com>
2021-10-31 19:26:19 +0600

    locals

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAq6xakfNwt0puwGVsDcN7Hpa5p8zoV4As",
-    authDomain: "testing-59fd8.firebaseapp.com",
-    projectId: "testing-59fd8",
-    storageBucket: "testing-59fd8.appspot.com",
-    messagingSenderId: "1008527264645",
-    appId: "1:1008527264645:web:4a21937a8b3ad1d84092ec"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 export default firebaseConfig;
